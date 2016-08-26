b34637fc4fbdd7a6f7646b9ae25774eaa480b08e
Yosvel Quintero <yosvelquintero@gmail.com>
2016-08-26 22:46:19 +0200

    Fixing auto-jump directive...

--- js/directives/auto-jump.directive.js
+++ js/directives/auto-jump.directive.js
@@ -9,17 +9,27 @@ define(['./module'], function(directives) {
         var maxLength = parseInt(attr.ngMaxlength);
         elem.on('keypress', function(e) {
           if (elem.val().length > maxLength - 2) {
-            var next = angular
+            var next;
+            var inputs = angular
               .element(document.body)
-              .find('[tabindex=' + (tabindex + 1) + ']');
-            if (next.length > 0) {
+              .find('input');
+            
+            angular.forEach(inputs, function(el) {
+              if ((tabindex + 1) == +el.getAttribute('tabindex')) {
+                next = el;
+              }
+            });
+            
+            if (next) {
               next.focus();
               return next.triggerHandler('keypress', {
                 which: e.which
               });
-            }
-            return false;
+            } else {
+              return false;              
+            }            
           }
+
           return true;
         });
       }
