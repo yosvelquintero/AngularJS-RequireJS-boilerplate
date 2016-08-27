982a03641920b365a38c837ce8368d8bfb0c60a1
Yosvel Quintero <yosvelquintero@gmail.com>
2016-08-27 10:23:04 +0200

    Changes on the directive autoJomp and improved html form input elements

--- js/directives/auto-jump.directive.js
+++ js/directives/auto-jump.directive.js
@@ -4,34 +4,16 @@ define(['./module'], function(directives) {
   directives.directive('autoJump', [function() {
     return {
       restrict: 'A',
-      link: function(scope, elem, attr, form) {
-        var tabindex = parseInt(attr.tabindex);
-        var maxLength = parseInt(attr.ngMaxlength);
-        elem.on('keypress', function(e) {
-          if (elem.val().length > maxLength - 2) {
-            var next;
-            var inputs = angular
-              .element(document.body)
-              .find('input');
-            
-            angular.forEach(inputs, function(el) {
-              if ((tabindex + 1) === +el.getAttribute('tabindex')) {
-                next = el;
-              }
-            });
-            
-            if (next) {
-              next.focus();
-              return next.triggerHandler({
-                type: 'keypress',
-                which: e.which
-              });
-            } 
-
-            return false;
+      link: function(scope, elem, attr) {
+        elem.on('input', function(e) {
+          var tabindex = parseInt(attr.tabindex);
+          var maxlength = parseInt(attr.maxlength);
+          if (elem.val().length === maxlength) {
+            var nextElement = document.querySelectorAll('#input' + (tabindex + 1));
+            if (nextElement.length) {
+              nextElement[0].focus();
+            }
           }
-
-          return true;
         });
       }
     }
